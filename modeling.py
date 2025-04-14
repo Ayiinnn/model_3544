215736d7e76533f947dc90090c932ce861c9db0f
Steve <158124586+Ayiinnn@users.noreply.github.com>
2025-04-14 16:15:45 +0800

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -51,10 +51,14 @@ class TemporalFusionTransformer(nn.Module):
         self.fin_embed = nn.Linear(17, config.d_model)         # 金融特征
         self.med_embed = nn.Linear(6, config.d_model)          # 媒体特征
         self.mkt_embed = nn.Linear(1, config.d_model)          # 市场情绪
+
+        #ce变换
+        self.ce_encoder = nn.LSTM(input_size = config.d_model, hidden_size = config.d_model, bidirectional = False)
         
         # 时序编码器
         self.lstm = nn.LSTM(config.d_model, config.d_model, num_layers=3)
         self.attention = nn.MultiheadAttention(config.d_model, num_heads=4)
+        
         
         # 输出层
         self.output_layer = nn.Linear(config.d_model, 1)       # 单点输出
@@ -79,12 +83,45 @@ class TemporalFusionTransformer(nn.Module):
 
 
         # 每个特征单独embedding形成 [B,T,d,d_model]?
-        # 不全部合并，而是分成senti_inp, target(复用金融第一个特征），fin_inp 即[B,T,d_senti,d_model],[B,T,d_model],[B,T,d_fin,d_model]?
-        # 另外是不是不应该加和而是沿特征维度拼接？例：torch.cat([med_emb,mkt_emb], dim=-2)  （-2对应[B,T,d,d_model]中的d）
+        # 不全部合并，而是分成senti_inp，fin_inp 即[B,T,d_senti,d_model],[B,T,d_fin,d_model]?
+        # 另外是不是不应该加和而是沿特征维度拼接？例：senti_inp = torch.cat([med_emb,mkt_emb], dim =-2)  （-2对应[B,T,d,d_model]中的d）
+
+        #嵌入后部分
+        #情绪编码
+        ce, ch, cc = self.senti_encoder(senti_inp)
+        ch, cc = ch.unsqueeze(0), cc.unsqueeze(0) 
+        ce , _ = self.ce_encoder(ce)              #LSTM
+
+        #金融编码
+        fin_features = self.simple_vsn(fin_inp)
+        fin, state = self.tem_encoder(fin_features, (ch, cc)) #LSTM
+
+        main_features = fin + self.input_gate(fin_features)  #skip_connection
+        main_features = self.input_gate_ln(main_features)
+
+        #融合(modifiedGRN)
+        enriched = self.enrichment_grn(temporal_features, c=ce)
+        
+        #后面把enriched输入Attention
         
         # LSTM + Attention
-        lstm_out, _ = self.lstm(combined.permute(1,0,2))      # [T, B, d_model]
+        lstm_out, _ = self.lstm(combined.permute(1,0,2))      # [T, B, d_model] #不需要吧
         attn_out, _ = self.attention(lstm_out, lstm_out, lstm_out)
+
+        
+        #还有position_wise grn(可选） 假设 attention输出x
+
+        x = self.attention_gate(x)
+        x = x + enriched
+        x = self.attention_ln(x)
+
+        x = self.positionwise_grn(x)
+
+        x = self.decoder_gate(x)
+        x = x + main_features
+        x = self.decoder_ln(x)
+
+        
         
         # 单点预测
         output = self.output_layer(attn_out[-1])              # 取最后一个时间步
